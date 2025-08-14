8268a5c7d2e9d4f4d424ab2ee08149cadb261f66
Carlos <cccarlos2727@gmail.com>
2025-08-14 17:35:32 +1000

    feature: created ReportPage(placeholder as stated in the ticket), updated the app.tsx for it

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -7,6 +7,7 @@ import HomePage from '@/pages/HomePage/HomePage';
 import SuburbReportPage from './pages/SuburbReportPage';
 import Layout from './components/Layout/Layout';
 import ExplorePage from '@/pages/ExplorePage/ExplorePage';
+import ReportPage from '@/pages/ReportPage/ReportPage';
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route path="/theme" element={<ThemeDemo />} />
           <Route index element={<HomePage />} />
+
+          <Route path="/suburb/:id" element={<ReportPage />} />
           <Route path="/explore/:location" element={<ExplorePage />} />
           <Route path="/suburb/:location" element={<SuburbReportPage />} />
         </Route>
