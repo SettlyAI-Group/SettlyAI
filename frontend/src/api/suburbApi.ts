7a65bb2742e2c7c4ccb4b5e9fb25e91c9cc828eb
shelton <damwcs@gmail.com>
2025-08-14 22:11:06 +1000

    format prettier

--- frontend/src/api/suburbApi.ts
+++ frontend/src/api/suburbApi.ts
@@ -2,13 +2,13 @@ import type { ISuburbReport } from '@/interfaces/suburbReport';
 import httpClient from './httpClient';
 import type { IDemandAndDev } from '@/interfaces/DemandAndDev';
 
-
 // Get suburb report by suburb ID
 export const getSuburbReport = async (
   suburbId: number
 ): Promise<ISuburbReport> => {
   const response = await httpClient.get<ISuburbReport>(`/suburb/${suburbId}`);
   return response.data;
+<<<<<<< HEAD
 };
 
 // Get Demand and Development data by suburb ID
@@ -16,3 +16,8 @@ export const getDemandAndDev = async (suburbId:number): Promise<IDemandAndDev> =
   const response = await httpClient.get<IDemandAndDev>(`/populationsupply/${suburbId}`);
   return response.data;
 };  
+||||||| parent of dafc419 (format prettier)
+};
+=======
+};
+>>>>>>> dafc419 (format prettier)
