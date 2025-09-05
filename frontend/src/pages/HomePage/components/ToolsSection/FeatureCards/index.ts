14a44dffa2a046e1b7130745cc0ae1f085db728c
gromov-hausdorff <uyfhggsf@protonmail.com>
2025-09-05 17:18:08 +1000

    Consolidate Feature Cards

---
+++ frontend/src/pages/HomePage/components/ToolsSection/FeatureCards/index.ts
@@ -0,0 +1,3 @@
+export { default as FeatureCards } from './FeatureCards';
+export { default as FeatureCardsSection } from './FeatureCardsSection';
+export { default as FeatureDetailSections } from './FeatureCardsSections';
