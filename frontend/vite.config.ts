874ec9db3ba02f1d5cc372d5da5d6ec474e9fc27
gromov-hausdorff <uyfhggsf@protonmail.com>
2025-10-07 23:36:47 +1100

    Export PDF in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_URL || 'http://localhost:5100',
+          target: env.VITE_API_URL,
           changeOrigin: true,
         },
       },
