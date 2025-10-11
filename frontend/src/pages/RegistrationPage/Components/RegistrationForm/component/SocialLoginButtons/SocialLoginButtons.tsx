9e9719ba688979ed7aaf3d0be74d53265c92b6c7
JOJOMRJ <163377337+JOJOMRJ@users.noreply.github.com>
2025-10-11 11:21:04 +0800

    Add Google, Facebook, and LinkedIn icons (#312)

--- frontend/src/pages/RegistrationPage/Components/RegistrationForm/component/SocialLoginButtons/SocialLoginButtons.tsx
+++ frontend/src/pages/RegistrationPage/Components/RegistrationForm/component/SocialLoginButtons/SocialLoginButtons.tsx
@@ -2,6 +2,9 @@
 import { Stack, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { signIn } from '@/utils/oidcClient';
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const GrayOutlinedButton = styled(Button)(({ theme }) => ({
   color: theme.palette.common.black, // 字体颜色
@@ -16,13 +19,28 @@ const CustomGrayOutlinedButton = styled(GrayOutlinedButton)(({ theme }) => ({
 export const SocialLoginButtons = () => {
   return (
     <Stack spacing={3} sx={{ width: '100%' }}>
-      <CustomGrayOutlinedButton fullWidth variant="outlined" onClick={() => signIn('google')}>
+      <CustomGrayOutlinedButton
+        fullWidth
+        variant="outlined"
+        onClick={() => signIn('google')}
+        startIcon={<GoogleIcon />}
+      >
         Sign In with Google
       </CustomGrayOutlinedButton>
-      <CustomGrayOutlinedButton fullWidth variant="outlined" onClick={() => signIn('facebook')}>
+      <CustomGrayOutlinedButton
+        fullWidth
+        variant="outlined"
+        onClick={() => signIn('facebook')}
+        startIcon={<FacebookIcon />}
+      >
         Sign In with Facebook
       </CustomGrayOutlinedButton>
-      <CustomGrayOutlinedButton fullWidth variant="outlined" onClick={() => signIn('linkedin')}>
+      <CustomGrayOutlinedButton
+        fullWidth
+        variant="outlined"
+        onClick={() => signIn('linkedin')}
+        startIcon={<LinkedInIcon />}
+      >
         Sign In with LinkedIn
       </CustomGrayOutlinedButton>
     </Stack>
